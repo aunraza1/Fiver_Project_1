fb65d55b526a2756738eb3be713e21077a8a4785
Aun Raza <aunraza03222@gmail.com>
2021-09-30 22:47:38 +0500

    More-Changes

--- src/screens/screen1.js
+++ src/screens/screen1.js
@@ -1,203 +1,203 @@
 import React from 'react'
-import 
-{
-    ScrollView,
-    View,
-    Text,
-    TouchableOpacity,
-    Image
+import {
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+
 }
-from 'react-native'
+  from 'react-native'
 import styles from './styles'
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import {faTrashAlt,faCopy} from '@fortawesome/free-regular-svg-icons'
-import {faChevronLeft,faLock,faExclamationTriangle,faChevronDown,faSearch,faSortAmountUpAlt} from '@fortawesome/free-solid-svg-icons'
-function Screen1(){
-    return(
-      <ScrollView style={styles.container}>
-          <View style={{flexDirection:'row'}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                  <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-         
-                 <FontAwesomeIcon style={{alignSelf:'center', height:18 ,width:11}}  icon={faChevronLeft}/>
-              
-                </TouchableOpacity>
-                
-              </View>
+import { faTrashAlt, faCopy } from '@fortawesome/free-regular-svg-icons'
+import { faChevronLeft, faLock, faExclamationTriangle, faChevronDown, faSearch, faSortAmountUpAlt } from '@fortawesome/free-solid-svg-icons'
 
-              <View  style={{justifyContent:'flex-end',marginRight:10}}>
-              <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-         
-         <FontAwesomeIcon style={{alignSelf:'center',}} size={15} icon={faLock}/>
-      
-        </TouchableOpacity>
-                  </View>
-                  <View  style={{justifyContent:'flex-end'}}>
-              <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-         
-         <FontAwesomeIcon style={{alignSelf:'center',}} size={15} icon={faTrashAlt}/>
-      
-        </TouchableOpacity>
-                  </View>
-                      
+function Screen1() {
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.row}>
+        <View style={styles.topIconLeft}>
+          <TouchableOpacity style={styles.btnBackContainer}>
+            <FontAwesomeIcon style={styles.backIcon} icon={faChevronLeft} />
+          </TouchableOpacity>
+        </View>
+
+        <View style={styles.topIconRight}>
+          <TouchableOpacity style={styles.btnBackContainer}>
+
+            <FontAwesomeIcon style={styles.center} size={15} icon={faLock} />
+
+          </TouchableOpacity>
+        </View>
+        <View style={styles.end}>
+          <TouchableOpacity style={styles.btnBackContainer}>
+
+            <FontAwesomeIcon style={styles.center} size={15} icon={faTrashAlt} />
+
+          </TouchableOpacity>
+        </View>
+
+
+      </View>
+      <View style={styles.subContainer}>
+        <View style={styles.topIconLeft}>
+          <Text style={styles.textStyle1}>Cameron Williamson</Text>
+          <View style={{ alignItems: 'flex-start', justifyContent: 'center' }}>
+            <Text style={styles.textStyle2} >$25 Claimed <Text style={{ color: '#2362FB', fontSize: 14, lineHeight: 24, fontFamily: "Inter-Thin Bold" }}> 0xc1912f...JSA59aE31 </Text>
+              <TouchableOpacity>
+                <FontAwesomeIcon size={20} icon={faCopy} />
+              </TouchableOpacity>
+
+            </Text>
+          </View>
+
+
+        </View>
+
+
+        <View style={styles.end}>
+          <Image style={styles.avatar} source={require('../assets/images/img1.jpg')} />
+
+
+        </View>
+      </View>
+      <View style={styles.boxContainer}>
+        <View style={styles.row}>
+          <FontAwesomeIcon style={{ marginTop: 2, marginRight: 5 }} icon={faExclamationTriangle} color="#EB5757" />
+          <Text style={styles.textStyle1}> Suspicious Activily Detected</Text>
+        </View>
+        <Text style={styles.boxTxt}>Beneficiary may be involved in suspicious activity.</Text>
+
+      </View>
+      <View style={styles.subContainer}>
+        <View style={styles.topIconLeft}>
+          <TouchableOpacity style={styles.btn} >
+            <Text style={styles.btnTxt}>All Transactions </Text>
+            <FontAwesomeIcon style={{ opacity: 0.7, height: 12, width: 7 }} icon={faChevronDown} />
+
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.end, { marginRight: 5 }]}>
+
+          <TouchableOpacity style={styles.btnBackContainer}>
+
+            <FontAwesomeIcon style={{ alignSelf: 'center', }} size={15} icon={faSearch} />
+
+          </TouchableOpacity>
+        </View>
+        <View style={[styles.end, { marginRight: 5 }]}>
+
+          <TouchableOpacity style={styles.btnBackContainer}>
+
+            <FontAwesomeIcon style={{ alignSelf: 'center', }} size={15} icon={faSortAmountUpAlt} />
+
+          </TouchableOpacity>
+        </View>
+        <View style={styles.end}>
+
+          <TouchableOpacity style={styles.btnBackContainer}>
+
+            <Image style={{ height: 20, width: 20, alignSelf: 'center' }} source={require('../assets/images/img2.jpg')} />
+
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={{ padding: 8 }}>
+        <View style={styles.subContainer}>
+          <View style={styles.topIconLeft}>
+            <View style={styles.row}>
+              <Text style={styles.textStyle1}>Sent</Text>
+              <FontAwesomeIcon size={13} style={{ marginTop: 4, marginLeft: 5 }} icon={faExclamationTriangle} color="#EB5757" />
+
+            </View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              <Text style={styles.textStyle2} >1 days ago. <Text style={{ color: '#2362FB', fontSize: 14, lineHeight: 24, fontFamily: "Inter-Thin Bold" }}> 0xc1912f...JSA59aE31 </Text> <TouchableOpacity>
+                <FontAwesomeIcon size={20} icon={faCopy} />
+              </TouchableOpacity>
+
+              </Text>
+            </View>
+
 
           </View>
-          <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Cameron Williamson</Text>
-                   <View style={{alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >Claimed <Text style={{color: '#2362FB',fontSize:14,lineHeight:24,fontFamily:"Inter-Thin Bold"}}> 0xc1912f...JSA59aE31 </Text> <TouchableOpacity>
-                    <FontAwesomeIcon size={10} icon ={faCopy}/>
-                  </TouchableOpacity>
-                     
-                  </Text>
-                  </View>
-                  
-                  
-                </View>
+          <View style={styles.end}>
+            <Text style={styles.textStyle1}>$3.5</Text>
+          </View>
+        </View>
+        <View style={styles.subContainer}>
+          <View style={[styles.container, { padding: 0 }]}>
+            <View style={styles.row}>
+              <Text style={styles.textStyle1}>Received</Text>
 
-          
-          <View style={{justifyContent:'flex-end'}}>
-              <Image style={{height:42,width:42,borderRadius:21}} source={require('../assets/images/img1.jpg')}/>
-              
+
+            </View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              <Text style={styles.textStyle2} >4 days ago. <Text style={styles.textStyle3}> 0xc1912f...JSA59aE31 </Text> <TouchableOpacity>
+                <FontAwesomeIcon size={20} icon={faCopy} />
+              </TouchableOpacity>
+
+              </Text>
+            </View>
+
 
           </View>
+          <View style={styles.end}>
+            <Text style={styles.textStyle1}>$2</Text>
           </View>
-          <View style={{height:110,borderRadius:8,borderWidth:2,borderColor:"#EB5757",padding:10,marginTop:20}}>
-              <View style={{flexDirection:'row'}}>
-                  <FontAwesomeIcon style={{marginTop:2,marginRight:5}} icon={faExclamationTriangle} color="#EB5757"/>
-                  <Text style={{fontFamily:"Manrope-Bold",fontSize:16,lineHeight:22}}> Suspicious Activily Detected</Text>
-              </View>
-                <Text style={{fontFamily:"Inter-Thin Bold",fontSize:15,lineHeight:24}}>Beneficiary may be involved in suspicious activity.</Text>
+        </View>
+        <View style={styles.subContainer}>
+          <View style={[styles.container, { padding: 0 }]}>
+            <View style={styles.row}>
+              <Text style={styles.textStyle1}>Claimed</Text>
+
+
+            </View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              <Text style={styles.textStyle2} >3 day ago.</Text>
+            </View>
+
 
           </View>
-          <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                  <TouchableOpacity style={{ justifyContent:'center', alignItems:'center', flexDirection:'row', width:169,height:36,backgroundColor:"#E9EDF4",borderRadius:46}} >
-                      <Text style={{width:115,height:28,fontFamily:'Inter-Thin Bold',lineHeight:28,fontSize:15}}>All Transactions </Text>
-                    <FontAwesomeIcon style={{opacity:0.7,height:12,width:7}}  icon={faChevronDown} />
-                         
-                  </TouchableOpacity>
-             </View>
-             <View style={{justifyContent:'flex-end',marginRight:5}}>
-             
-              <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-         
-         <FontAwesomeIcon style={{alignSelf:'center',}} size={15} icon={faSearch}/>
-      
-        </TouchableOpacity>
-                  </View>
-                  <View style={{justifyContent:'flex-end',marginRight:5}}>
-             
-             <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-        
-        <FontAwesomeIcon style={{alignSelf:'center',}} size={15} icon={faSortAmountUpAlt}/>
-     
-       </TouchableOpacity>
-                 </View>
-                 <View style={{justifyContent:'flex-end'}}>
-             
-             <TouchableOpacity  style={{height:33.6,width:33.6,borderRadius:33.6/2,backgroundColor:'#E9EDF4',justifyContent:'center'}}>
-        
-        <FontAwesomeIcon style={{alignSelf:'center',}} size={15} icon={faTrashAlt}/>
-     
-       </TouchableOpacity>
-                 </View>
-             </View>   
-             <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                   <View style={{flexDirection:'row'}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Sent</Text>
-                  <FontAwesomeIcon size={13} style={{marginTop:4,marginLeft:5}} icon={faExclamationTriangle} color="#EB5757"/>
+          <View style={styles.end}>
+            <Text style={styles.textStyle1}>$0.5</Text>
+          </View>
+        </View>
+        <View style={styles.subContainer}>
+          <View style={styles.topIconLeft}>
+            <View style={styles.row}>
+              <Text style={styles.textStyle1}>Claimed</Text>
 
-                  </View>
-                   <View style={{justifyContent:'flex-start',alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >1 days ago. <Text style={{color: '#2362FB',fontSize:14,lineHeight:24,fontFamily:"Inter-Thin Bold"}}> 0xc1912f...JSA59aE31 </Text> <TouchableOpacity>
-                    <FontAwesomeIcon size={20} icon ={faCopy}/>
-                  </TouchableOpacity>
-                     
-                  </Text>
-                  </View>
-                  
-                  
-                </View>
-                <View style={{justifyContent:'flex-end'}}>
-                    <Text style={{marginBottom:8,fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>$3.5</Text>
-                 </View>   
-                </View>
-                <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                   <View style={{flexDirection:'row'}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Received</Text>
-                 
 
-                  </View>
-                   <View style={{justifyContent:'flex-start',alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >4 days ago. <Text style={{color: '#2362FB',fontSize:14,lineHeight:24,fontFamily:"Inter-Thin Bold"}}> 0xc1912f...JSA59aE31 </Text> <TouchableOpacity>
-                    <FontAwesomeIcon size={20} icon ={faCopy}/>
-                  </TouchableOpacity>
-                     
-                  </Text>
-                  </View>
-                  
-                  
-                </View>
-                <View style={{justifyContent:'flex-end'}}>
-                    <Text style={{marginBottom:8,fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>$2</Text>
-                 </View>   
-                </View>
-                <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                   <View style={{flexDirection:'row'}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Claimed</Text>
-                 
+            </View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              <Text style={styles.textStyle2} >3 day ago.</Text>
+            </View>
 
-                  </View>
-                   <View style={{justifyContent:'flex-start',alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >3 day ago.</Text>
-                  </View>
-                  
-                  
-                </View>
-                <View style={{justifyContent:'flex-end'}}>
-                    <Text style={{marginBottom:8,fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>$0.5</Text>
-                 </View>   
-                </View>
-                <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                   <View style={{flexDirection:'row'}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Claimed</Text>
-                 
 
-                  </View>
-                   <View style={{justifyContent:'flex-start',alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >3 day ago.</Text>
-                  </View>
-                  
-                  
-                </View>
-                <View style={{justifyContent:'flex-end'}}>
-                    <Text style={{marginBottom:8,fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>$0.5</Text>
-                 </View>   
-                </View>
-                <View style={{flexDirection:'row',marginTop:20}}>
-              <View style={{justifyContent:'flex-start',flex:1}}>
-                   <View style={{flexDirection:'row'}}>
-                  <Text style={{fontFamily:'Manrope-Bold',fontSize:16,lineHeight:22}}>Joined Community</Text>
-                 
+          </View>
+          <View style={styles.end}>
+            <Text style={styles.textStyle1}>$0.5</Text>
+          </View>
+        </View>
+        <View style={styles.subContainer}>
+          <View style={styles.topIconLeft}>
+            <View style={styles.row}>
+              <Text style={styles.textStyle1}>Joined Community</Text>
 
-                  </View>
-                   <View style={{justifyContent:'flex-start',alignItems:'flex-start'}}>
-                  <Text style={{fontFamily:"Inter-Thin",fontSize:14,lineHeight:24}} >6 days ago. Added by Afonso Barbosa</Text>
-                  </View>
-                  
-                  
-                </View>
-                 
-                </View>
-          
 
-      </ScrollView>
-    )
+            </View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+              <Text style={styles.textStyle2} >6 days ago. Added by Afonso Barbosa</Text>
+            </View>
+
+
+          </View>
+        </View>
+      </View>
+
+
+    </ScrollView>
+  )
 }
 export default Screen1
--- src/screens/styles.js
+++ src/screens/styles.js
@@ -2,11 +2,94 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
+        flexGrow:1,
         backgroundColor:'#FFFFFF',
-        padding:15
-
+        padding:10
+    },
+    row:{
+        flexDirection:'row'
+    },
+    topIconLeft:{
+        justifyContent:'flex-start',flex:1
+    },
+    btnBackContainer:{
+        height:33.6,
+        width:33.6,
+        borderRadius:33.6/2,
+        backgroundColor:'#E9EDF4',
+        justifyContent:'center'
+    },
+    backIcon:{
+        alignSelf:'center',
+         height:18
+         ,width:11
     }
+    ,topIconRight:{
+        justifyContent:'flex-end'
+        ,marginRight:10
+    },
+    center:{
+        alignSelf:'center'
+
+    },
+    end:{
+        justifyContent:'flex-end'
+    },
+    subContainer:{
+        flexDirection:'row'
+        ,marginTop:20
+    },
+    textStyle1:{
+        fontFamily:'Manrope-Bold'
+        ,fontSize:16,
+        lineHeight:22
+
+    },
+    textStyle2:{
+        fontFamily:'Inter-Thin'
+        ,fontSize:14,
+        lineHeight:24
+
+    },
+    textStyle3:{
+        color: '#2362FB'
+        ,fontSize:14,
+        lineHeight:24,
+        fontFamily:"Inter-Thin Bold"
+    },
+    boxContainer:{
+        height:110,
+        borderRadius:8,
+        borderWidth:2,
+        borderColor:"#EB5757",
+        padding:10,
+        marginTop:20
+    },
+    avatar:{
+        height:42,
+        width:42,
+        borderRadius:21
+    },boxTxt:{
+        fontFamily:"Inter-Thin Bold",
+        fontSize:15,
+        lineHeight:24
+    },btnTxt:{
+        width:115,
+        height:28,
+        fontFamily:'Inter-Thin Bold',
+        lineHeight:28,
+        fontSize:15
+    },
+    btn:{
+        justifyContent:'center',
+         alignItems:'center',
+          flexDirection:'row',
+           width:169,
+           height:36,
+           backgroundColor:"#E9EDF4",
+           borderRadius:46
+    }
+    
 
 })
 export default styles
